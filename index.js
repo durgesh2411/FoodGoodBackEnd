457dbb4785f36b8ab3bfa8ca1df1ed48ff75ac7a
durgesh2411 <durgesh.vns24@gmail.com>
2025-06-03 11:25:03 +0530

    commit first

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-import { app } from "./src/app.js";
-import connectDB from "./src/db/index.js";
-import dotenv from "dotenv";
-import { sendSMS } from "./src/utils/smsUtil.js";
-dotenv.config({ path: "./.env" });
+// import { app } from "./src/app.js";
+// import connectDB from "./src/db/index.js";
+// import dotenv from "dotenv";
+// import { sendSMS } from "./src/utils/smsUtil.js";
+// dotenv.config({ path: "./.env" });
 
 // connectDB()
 //   .then(() => {
@@ -11,31 +11,15 @@ dotenv.config({ path: "./.env" });
 //     });
 //   })
 //   .then(() => {
-//     app.listen(process.env.PORT || 8000, () => {
-//       console.log(`Server running at ${process.env.PORT}`);
+//     const PORT = process.env.PORT || 8000;
+//     app.listen(PORT, () => {
+//       console.log(`Server running at http://localhost:${PORT}`);
 //     });
 //   })
 //   .catch((err) => {
 //     console.log("MongoDB connection failed!!! ", err);
 //     process.exit(1);
 //   });
-
-connectDB()
-  .then(() => {
-    app.on("error", (error) => {
-      console.log("express error", error);
-    });
-  })
-  .then(() => {
-    const PORT = process.env.PORT || 8000;
-    app.listen(PORT, () => {
-      console.log(`Server running at http://localhost:${PORT}`);
-    });
-  })
-  .catch((err) => {
-    console.log("MongoDB connection failed!!! ", err);
-    process.exit(1);
-  });
 
 
 // sendMail();
@@ -58,3 +42,41 @@ connectDB()
 //     throw error;
 //   }
 // })();
+
+
+
+import { app } from "./src/app.js";
+import connectDB from "./src/db/index.js";
+import dotenv from "dotenv";
+dotenv.config({ path: "./.env" });
+
+let isConnected = false;
+
+// Vercel serverless handler export
+export default async function handler(req, res) {
+  if (!isConnected) {
+    await connectDB();
+    isConnected = true;
+  }
+  return app(req, res);
+}
+
+// For local development only: run app.listen if not running on Vercel
+if (process.env.VERCEL !== "1") {
+  connectDB()
+    .then(() => {
+      app.on("error", (error) => {
+        console.log("express error", error);
+      });
+    })
+    .then(() => {
+      const PORT = process.env.PORT || 8000;
+      app.listen(PORT, () => {
+        console.log(`Server running at http://localhost:${PORT}`);
+      });
+    })
+    .catch((err) => {
+      console.log("MongoDB connection failed!!! ", err);
+      process.exit(1);
+    });
+}
--- src/middlewares/multer.middleware.js
+++ src/middlewares/multer.middleware.js
@@ -1,8 +1,25 @@
+// import multer from "multer";
+
+// const storage = multer.diskStorage({
+//   destination: function (req, file, cb) {
+//     cb(null, "./public/temp");
+//   },
+//   filename: function (req, file, cb) {
+//     cb(null, file.originalname);
+//   },
+// });
+
+// export const upload = multer({
+//   storage,
+// });
+
 import multer from "multer";
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "./public/temp");
+    // Use /tmp on Vercel, ./public/temp locally
+    const dest = process.env.VERCEL === "1" ? "/tmp" : "./public/temp";
+    cb(null, dest);
   },
   filename: function (req, file, cb) {
     cb(null, file.originalname);
